e6d3172b150c7432ef37bfecb04635d02b576ca4
Txiki <iam@weird.es>
2014-01-13 17:23:15 +0100

    index

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>dem0</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="dem0">
+    <meta name="author" content="www.weird.es">
+
+    <!-- Le styles -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    
+   
+    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+   
+    <link href="/css/bootstrap-modal.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+	Hello
+  </body>
+</html>
